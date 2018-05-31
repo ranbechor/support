0048015ea0c1a38ddf6d523e3f6fc0630e0fabb8
ranbechor <37666549+ranbechor@users.noreply.github.com>
2018-05-30 20:28:49 -0400

    Update create-case.php

--- php/create-case.php
+++ php/create-case.php
@@ -1,34 +1,62 @@
-
-
 <?php
+$subject = 'My subject';
+$description='this is very serious';
+$Priority='low';
+$appUrl='cloud.perfectomobile.com';
+$suppliedName='ran';
+$suppliedEmail='perfecto@perfectomobie.com';
+$SuppliedPhone = '+111111111111';
+$filename ='file.txt';
+$path ='ATTACHMENT.txt';
 
 $SfConfig = parse_ini_file("conf.ini");
 define(SECURITY_TOKEN, $SfConfig['token']);
 define(USERNAME, $SfConfig['user']);
 define(PASSWORD, $SfConfig['password']);
-require_once('soapclient/SforceEnterpriseClient.php');
+require_once ('soapclient/SforceEnterpriseClient.php');
 
 $mySforceConnection = new SforceEnterpriseClient();
 $mySoapClient = $mySforceConnection->createConnection("soapclient/perfectomobile.xml");
 $mylogin = $mySforceConnection->login(USERNAME,PASSWORD.SECURITY_TOKEN);
 
-#case fields
+#case fields 
 $sObject = new stdclass();
-$sObject->Subject = "some subject";
-$sObject->Description = "some description";
-$sObject->Priority = "low";
-$sObject->AppURL__c = "my cloud.perfectomobile.com";
-$sObject->SuppliedName = "first name + last name ";
-$sObject->SuppliedEmail = "fish@perfectomobile.com";
-$sObject->SuppliedPhone = "+219829182";
+$sObject->Subject = $subject;
+$sObject->Description =$description;
+$sObject->Priority =$Priority; 
+$sObject->AppURL__c =$appUrl;
+$sObject->SuppliedName =$suppliedName;
+$sObject->SuppliedEmail =$suppliedEmail;
+$sObject->SuppliedPhone =$SuppliedPhone; 
 
+#creating the case
 try {
-  $response = $mySforceConnection->create(array($sObject), 'Case');
-  print_r($response);
-  exit;
-} catch (Exception $e) {
-  echo $mySforceConnection->getLastRequest();
-  echo $e->faultstring;
-  exit;
+   $response = $mySforceConnection->create(array($sObject), 'Case');
+   foreach ($response as $record)$parent=$record->id;
+   $json['case'] = $response;
 }
+catch (Exception $e){ 
+
+    $json['case'] = NULL;
+    #create case failed 
+}
+if (file_exists ($path) ){
+#uploading attachment
+    try {
+        $sfObj = new stdClass();
+        $data= file_get_contents($path);
+        $sfObj->Body = base64_encode($data);
+        $sfObj->Name = $filename;
+        $sfObj->ParentId = $parent;
+        
+        $response = $mySforceConnection->create(array($sfObj), 'Attachment');
+        $json['attachment'] = $response;
+    }
+    catch (Exception $e){
+        $json['attachment'] = NULL;
+        }
+    }
+    $myJSON = json_encode($json);
+    print_r($json);
+
  ?>
